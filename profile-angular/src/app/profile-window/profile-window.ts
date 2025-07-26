aa599c9271c3f2962d117f77a7a2c01f8229aa9e
astralvoid-dev <149061265+astralvoid-dev@users.noreply.github.com>
2025-07-26 22:27:18 +0200

    Startpage V1, Bio updated

--- profile-angular/src/app/profile-window/profile-window.ts
+++ profile-angular/src/app/profile-window/profile-window.ts
@@ -8,5 +8,7 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   styleUrl: './profile-window.css'
 })
 export class ProfileWindow {
-
+  birthDate = new Date('2003-02-23');
+  timeDiff = Math.abs(Date.now() - this.birthDate.getTime());
+  age = Math.floor((this.timeDiff / (1000 * 3600 * 24))/365.25);
 }
